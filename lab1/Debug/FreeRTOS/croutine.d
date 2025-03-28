2b758b6b0e12c1d135531c9710c952ff623d17ba
Petakuo <162006888+Petakuo@users.noreply.github.com>
2025-03-29 00:30:25 +0800

    Add files via upload

---
+++ lab1/Debug/FreeRTOS/croutine.d
@@ -0,0 +1,21 @@
+FreeRTOS/croutine.o: ../FreeRTOS/croutine.c \
+ C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/projdefs.h \
+ C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/portable.h \
+ C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/User/Desktop/STM32/lab1/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/task.h \
+ C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/list.h \
+ C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/croutine.h
+C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/FreeRTOS.h:
+C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/FreeRTOSConfig.h:
+C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/projdefs.h:
+C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/portable.h:
+C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/User/Desktop/STM32/lab1/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/task.h:
+C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/list.h:
+C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/croutine.h:
---
+++ lab1/Debug/FreeRTOS/list.d
@@ -0,0 +1,17 @@
+FreeRTOS/list.o: ../FreeRTOS/list.c \
+ C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/projdefs.h \
+ C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/portable.h \
+ C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/User/Desktop/STM32/lab1/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/list.h
+C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/FreeRTOS.h:
+C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/FreeRTOSConfig.h:
+C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/projdefs.h:
+C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/portable.h:
+C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/User/Desktop/STM32/lab1/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/User/Desktop/STM32/lab1/FreeRTOS/include/list.h:
