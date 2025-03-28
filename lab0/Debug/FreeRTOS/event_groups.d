fe17e184a43e5d432532d237603e99e108c8a810
Petakuo <162006888+Petakuo@users.noreply.github.com>
2025-03-29 00:18:08 +0800

    Add files via upload

---
+++ lab0/Debug/FreeRTOS/event_groups.d
@@ -0,0 +1,27 @@
+FreeRTOS/event_groups.o: ../FreeRTOS/event_groups.c \
+ C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/projdefs.h \
+ C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/portable.h \
+ C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/User/Desktop/STM32/lab0/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/task.h \
+ C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/list.h \
+ C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/timers.h \
+ C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/task.h \
+ C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/event_groups.h \
+ C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/timers.h
+C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/FreeRTOS.h:
+C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/FreeRTOSConfig.h:
+C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/projdefs.h:
+C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/portable.h:
+C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/User/Desktop/STM32/lab0/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/task.h:
+C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/list.h:
+C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/timers.h:
+C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/task.h:
+C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/event_groups.h:
+C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/timers.h:
---
+++ lab0/Debug/FreeRTOS/queue.d
@@ -0,0 +1,23 @@
+FreeRTOS/queue.o: ../FreeRTOS/queue.c \
+ C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/projdefs.h \
+ C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/portable.h \
+ C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/User/Desktop/STM32/lab0/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/task.h \
+ C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/list.h \
+ C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/queue.h \
+ C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/task.h
+C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/FreeRTOS.h:
+C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/FreeRTOSConfig.h:
+C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/projdefs.h:
+C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/portable.h:
+C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/User/Desktop/STM32/lab0/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/task.h:
+C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/list.h:
+C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/queue.h:
+C:/Users/User/Desktop/STM32/lab0/FreeRTOS/include/task.h:
